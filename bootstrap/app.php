d27ec4da9262d5a4d52e328450895de255d2c98a
SubZiwo <risdon.polvito@cvsu.edu.ph>
2026-07-15 01:22:48 +0800

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,10 +5,10 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
-return Application::configure(basePath: dirname(_DIR_))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: _DIR_.'/../routes/web.php',
-        commands: _DIR_.'/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
